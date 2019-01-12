733d6b1f1c213a80796fd1587e10e2951aeaef20
Andrey Bogdan <bezova@gmail.com>
2019-01-12 00:31:07 -0600

    added test

---
+++ tests/test_layers.py
@@ -0,0 +1,26 @@
+import numpy as np
+from nnlib.layers import Tanh
+from nnlib.tensor import Tensor
+
+import pytest
+
+
+@pytest.mark.parametrize("xx", [
+    ([0]),
+    ([0,1]),
+    ([[0,1],[2,3]])
+])
+
+
+@pytest.fixture(params=[[0], [0,2]])
+def X(request):
+    x = np.array(request.param)
+    return  x
+
+
+def test_tanh(X):
+    tan = Tanh()
+
+    out = tan.forawar(X)
+
+    assert np.allclose(out, np.tanh(X))
